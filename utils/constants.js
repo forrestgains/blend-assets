05986c2afd578dff02cc1d5b68bfc39564771ed8
forrestgains <forrestgains1@gmail.com>
2023-01-28 12:35:54 -0600

    add canto tokens

---
+++ utils/constants.js
@@ -0,0 +1,29 @@
+const overrides = [
+  {
+    id: "DASH_DASH.null",
+    blockchain: "DASH",
+    symbol: "DASH",
+    image: "https://forrestgains.github.io/icons/dash.png",
+    address: null,
+    decimals: 8,
+    name: null,
+  },
+  {
+    id: "ZCASH_ZCASH.null",
+    blockchain: "ZCASH",
+    symbol: "ZCASH",
+    image: "https://forrestgains.github.io/icons/zcash.png",
+    address: null,
+    decimals: 8,
+    name: null,
+  },
+  {
+    id: "CANTO_CANTO.null",
+    blockchain: "CANTO",
+    symbol: "CANTO",
+    image: "https://forrestgains.github.io/icons/canto.png",
+    address: null,
+    decimals: 18,
+    name: null,
+  },
+];
