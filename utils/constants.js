28950f138099337053f1748c788a2836489e1bad
forrestgains <forrestgains1@gmail.com>
2023-01-28 13:58:51 -0600

    test canto tokens

--- utils/constants.js
+++ utils/constants.js
@@ -26,4 +26,22 @@ const overrides = [
     decimals: 18,
     name: null,
   },
+  {
+    id: "CANTO_NOTE.0x4e71A2E537B7f9D9413D3991D37958c0b5e1e503",
+    blockchain: "CANTO",
+    symbol: "NOTE",
+    image: "https://forrestgains.github.io/icons/note.png",
+    address: "0x4e71A2E537B7f9D9413D3991D37958c0b5e1e503",
+    decimals: 18,
+    name: null,
+  },
+  {
+    id: "CANTO_wCANTO.0x826551890Dc65655a0Aceca109aB11AbDbD7a07B",
+    blockchain: "CANTO",
+    symbol: "wCANTO",
+    image: "https://forrestgains.github.io/icons/canto.png",
+    address: "0x826551890Dc65655a0Aceca109aB11AbDbD7a07B",
+    decimals: 18,
+    name: null,
+  },
 ];
